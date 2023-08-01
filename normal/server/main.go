4fcb55b58f608bc1e69c7748b25d7d30ef4a5fd9
zhitkov.t <zhitkov.t@backspark.ru>
2023-08-01 20:02:41 +0300

    iniy

---
+++ normal/server/main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"bufio"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"strings"
+)
+
+const difficulty = 4  // difficult for PoW
+const word = "naruto" // base word
+
+func main() {
+	//create a Listener
+	listener, err := net.Listen("tcp", "0.0.0.0:8080")
+	if err != nil {
+		fmt.Println("listening error:", err.Error())
+		return
+	}
+	defer listener.Close()
+
+	fmt.Println("server is working...")
+
+	//endless loop for handling connections
+	for {
+
+		//try to create one connection
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Ошибка при принятии подключения:", err.Error())
+			return
+		}
+
+		//if its ok go for hadling the connection in own thread
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	fmt.Println("Клиент подключен:", conn.RemoteAddr())
+
+	//create objects for reading and writing
+	reader := bufio.NewReader(conn)
+	writer := bufio.NewWriter(conn)
+
+	//write welcome message with w - object
+	writer.WriteString("Connection created \n")
+	writer.Flush()
+
+	//endless loop for getting and sending messages with a client
+	for {
+
+		//send message for client
+		writer.WriteString("Enter a code\n") // не выводит в клиенте а значи сервер криво отдает
+		writer.Flush()
+
+		//read answer from client
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Ошибка при чтении данных:", err.Error())
+			return
+		}
+		data := strings.TrimSpace(input)
+		fmt.Println(data)
+
+		//figuring out if he had done work
+		isRight := nonceIsRight("naruto", 4, data)
+
+		//if hashes are not the same
+		if !isRight {
+			writer.WriteString(fmt.Sprintf("Проверка не пройдена\n"))
+			writer.Flush()
+			continue
+		}
+
+		//tell him he is a good boy
+		writer.WriteString(fmt.Sprintf("Проверка пройдена !!!!!\n"))
+		writer.Flush()
+
+		//show the truth
+		writer.WriteString(fmt.Sprintf("get the wisdom....rand\n"))
+		writer.WriteString(getQuote() + "\n")
+		writer.Flush() // Flush the writer to send the response immediately
+	}
+}
+
+// for checking the PoW
+func nonceIsRight(word string, difficulty int, nonce string) bool {
+
+	hash := sha256.Sum256([]byte(fmt.Sprintf("%s%s", word, nonce)))
+	hashStr := hex.EncodeToString(hash[:])
+
+	if strings.HasPrefix(hashStr, strings.Repeat("0", difficulty)) {
+		return true
+	}
+
+	return false
+}
+
+func getQuote() string {
+	url := "https://zenquotes.io/?api=quotes"
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Println("error qoutes reading:", err.Error())
+		return ""
+	}
+	defer response.Body.Close()
+	data, _ := io.ReadAll(response.Body)
+	var quotes []interface{}
+	err = json.Unmarshal(data, &quotes)
+	if err != nil {
+		fmt.Println("error quotes parsing:", err.Error())
+		return ""
+	}
+	firstQuote := quotes[0].(map[string]interface{})
+	return firstQuote["q"].(string)
+}
---
+++ simple/client.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bufio"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	//create connection to a server
+	conn, err := net.Dial("tcp", "127.0.0.1:8080")
+	if err != nil {
+		fmt.Println("Ошибка при подключении:", err.Error())
+		return
+	}
+	defer conn.Close()
+
+	//get the response of the server
+	go readServerResponses(conn)
+
+	//creating a reader from console and writer for connection
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(conn)
+
+	//read data from console
+	for {
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading the input:", err.Error())
+			return
+		}
+
+		//write message to a server
+		writer.WriteString(input)
+		writer.Flush()
+
+	}
+
+}
+
+// reading response from server
+func readServerResponses(conn net.Conn) {
+	reader := bufio.NewReader(conn)
+
+	for {
+		response, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("error getting the response:", err.Error())
+			return
+		}
+
+		fmt.Println(response)
+	}
+}
+
+// for Proving the Work
+func calculatePoW(data string, difficulty int) int {
+	nonce := 0
+	for {
+		hash := sha256.Sum256([]byte(fmt.Sprintf("%s%d", data, nonce)))
+		hashStr := hex.EncodeToString(hash[:])
+
+		if strings.HasPrefix(hashStr, strings.Repeat("0", difficulty)) {
+			return nonce
+		}
+
+		nonce++
+	}
+}
